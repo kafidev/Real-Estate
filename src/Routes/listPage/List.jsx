052799bfdc54917c68cb0abd965bf08ee39bdaa3
kafidev <abdikafiyusuf599@gmail.com>
2026-07-11 23:41:18 +0300

    adding lists

---
+++ src/Routes/listPage/List.jsx
@@ -0,0 +1,14 @@
+import "./listPage.scss";
+
+const List = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <ul>
+        <li>soo dhawow</li>
+      </ul>
+    </div>
+  )
+}
+
+export default List
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,19 +1,45 @@
 import "./searchBar.scss";
+import { useState } from "react";
+
+const types = ["buy", "rent"];
 
 const SearchBar = () => {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (val) => {
+     setQuery((prev) =>({...prev , type:val}))
+
+  };
+
+
   return (
     <div className="searchBar">
       <div className="type">
-        <button>Buy</button>
-        <button>Rent</button>
+        {types.map((type) => (
+          <button
+           key={type} onClick={() => switchType(type)}
+            className={query.type == type ? "active" : ""}
+           >
+            {type}
+            
+          </button>
+        ))}
       </div>
       <form>
         <input type="text" placeholder="city Location" />
         <input type="number" min={0} max={1000000} placeholder="Min Price" />
         <input type="number" min={0} max={1000000} placeholder="Max Price" />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchBar;
